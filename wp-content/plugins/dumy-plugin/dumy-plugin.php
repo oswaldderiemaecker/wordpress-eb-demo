b821670969e394d04261b8479bbc750c44292626
Oswald De Riemaecker <oswald@continuousphp.com>
2017-02-18 19:04:13 +0100

    Adding dumy-plugin

---
+++ wp-content/plugins/dumy-plugin/dumy-plugin.php
@@ -0,0 +1,10 @@
+<?php
+    /*
+    Plugin Name: dumy-plugin
+    Plugin URI:
+    Description: A Dumy plugin
+    Author: John Doe
+    Version: 1.1
+    Author URI: https://www.johndoe-dumy-plugin.com
+    */
+?>
